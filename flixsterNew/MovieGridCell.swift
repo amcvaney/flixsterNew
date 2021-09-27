77c8e02e2eafa506db0a9fd7587f5d001a99d717
Abigayle McVaney <abirosemcvaney@gmail.com>
2021-09-26 20:19:37 -0500

    COMMIT

---
+++ flixsterNew/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixsterNew
+
+//
+//  Created by Abigayle Mcvaney on 9/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
